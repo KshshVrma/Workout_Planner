3470f78a8a93d2d37dcb1b7b3c04d3796b92a992
Kashish <kashishverma8382@gmail.com>
2022-11-04 09:13:32 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './App.css';
+import{Route,Routes}from 'react-router-dom';
+import{Box}from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from './pages/Home'
+import ExerciseDetail from './pages/ExerciseDetail';
 const App = () => {
   return (
-    <div>App</div>
+   <Box width="400px">
+   <Navbar></Navbar>
+    <Routes>
+<Route path="/" element={<Home></Home>}></Route>
+<Route path=".exercise/:id" element={<ExerciseDetail/>}></Route>
+
+    </Routes>
+   </Box>
   )
 }
 
