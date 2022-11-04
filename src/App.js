15eed7da26bc3746e078e660462ad179fac53994
Kashish <kashishverma8382@gmail.com>
2022-11-04 10:32:29 +0530

    .

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import{Box}from '@mui/material';
 import Navbar from './components/Navbar';
 import Home from './pages/Home'
 import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
 const App = () => {
   return (
-   <Box width="400px">
+   <Box width="400px" sx={{width:{x1:'1480px'}}}m="auto">
    <Navbar></Navbar>
     <Routes>
 <Route path="/" element={<Home></Home>}></Route>
 <Route path=".exercise/:id" element={<ExerciseDetail/>}></Route>
-
+hello
     </Routes>
+    <Footer></Footer>
    </Box>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import{Stack}from '@mui/material';
+import Logo from '../assets/images/Logo.png';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Stack>
+<Link to="/">
+
+<img src={Logo} 
+alt ="logo " style ={
+{width:'48px',height:'48px',margin:'0 20px'}
+}
+/>
+
+
+</Link>
+<Stack>
+<Link to="/">
+    Home
+   
+  </Link>
+  <a herf="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exercises</a>
+</Stack>
+
+    </Stack>
   )
 }
 
