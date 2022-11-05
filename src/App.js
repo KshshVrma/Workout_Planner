fbe32c87c7a4652326aa4584e364bdebead2dd75
Kashish <kashishverma8382@gmail.com>
2022-11-05 09:34:37 +0530

    pre-dep

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import './App.css';
-import{Route,Routes}from 'react-router-dom';
-import{Box}from '@mui/material';
-import Navbar from './components/Navbar';
-import Home from './pages/Home'
-import ExerciseDetail from './pages/ExerciseDetail';
-import Footer from './components/Footer';
-const App = () => {
-  return (
-   <Box width="400px" sx={{width:{x1:'1480px'}}}m="auto">
-   <Navbar></Navbar>
-    <Routes>
-<Route path="/" element={<Home></Home>}></Route>
-<Route path=".exercise/:id" element={<ExerciseDetail/>}></Route>
-hello
-    </Routes>
-    <Footer></Footer>
-   </Box>
-  )
-}
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-export default App
+import './App.css';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
+
+export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import {useState}from 'react';
-import HeroBanner from '../components/HeroBanner';
-import SearchExercises from '../components/SearchExercises';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
+
 import Exercises from '../components/Exercises';
-import {Box}from '@mui/material';
+import SearchExercises from '../components/SearchExercises';
+import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
   return (
     <Box>
-<Box>
-  <HeroBanner/>
-  <SearchExercises/>
-  <Exercises/>
-</Box>
-
+      <HeroBanner />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
